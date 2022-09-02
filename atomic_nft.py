876911707edcba1e497d466ff73ca0597fef139c
Phil D <phil@linux.com>
2022-09-02 14:51:28 -0400

    total refactor

--- atomic_nft.py
+++ atomic_nft.py
@@ -1,64 +1,10 @@
 from PIL import Image
 import math, random
-import matplotlib.pyplot as plt
 from samila import GenerativeImage, Projection
 import os
-
-def rand_cmap(nlabels, type='bright', first_color_black=True, last_color_black=False, verbose=True):
-    """
-    Creates a random colormap to be used together with matplotlib. Useful for segmentation tasks
-    :param nlabels: Number of labels (size of colormap)
-    :param type: 'bright' for strong colors, 'soft' for pastel colors
-    :param first_color_black: Option to use first color as black, True or False
-    :param last_color_black: Option to use last color as black, True or False
-    :param verbose: Prints the number of labels and shows the colormap. True or False
-    :return: colormap for matplotlib
-    """
-    from matplotlib.colors import LinearSegmentedColormap
-    import colorsys
-    import numpy as np
+from cmap_utils import get_random_cmap
 
 
-    if type not in ('bright', 'soft'):
-        print ('Please choose "bright" or "soft" for type')
-        return
-
-    if verbose:
-        print('Number of labels: ' + str(nlabels))
-
-    # Generate color map for bright colors, based on hsv
-    if type == 'bright':
-        randHSVcolors = [(np.random.uniform(low=0.0, high=1),
-                          np.random.uniform(low=0.2, high=1),
-                          np.random.uniform(low=0.9, high=1)) for i in range(nlabels)]
-
-        # Convert HSV list to RGB
-        randRGBcolors = []
-        for HSVcolor in randHSVcolors:
-            randRGBcolors.append(colorsys.hsv_to_rgb(HSVcolor[0], HSVcolor[1], HSVcolor[2]))
-
-        if first_color_black:
-            randRGBcolors[0] = [0, 0, 0]
-
-        if last_color_black:
-            randRGBcolors[-1] = [0, 0, 0]
-
-
-    # Generate soft pastel colors, by limiting the RGB spectrum
-    if type == 'soft':
-        low = 0.6
-        high = 0.95
-        randRGBcolors = [(np.random.uniform(low=low, high=high),
-                          np.random.uniform(low=low, high=high),
-                          np.random.uniform(low=low, high=high)) for i in range(nlabels)]
-
-        if first_color_black:
-            randRGBcolors[0] = [0, 0, 0]
-
-        if last_color_black:
-            randRGBcolors[-1] = [0, 0, 0]
-
-    return randRGBcolors
 
 def f1(x, y):
     result = random.uniform(-1,1) * x**2  - math.sin(y**2) + abs(y-x)
@@ -69,31 +15,35 @@ def f2(x, y):
     return result
 
 
-for nft_num in range(0, 200):
-    os.system("mkdir -p nfts/%d" % (nft_num))
-    new_cmap = rand_cmap(10, type=random.choice(['soft', 'bright']), first_color_black=False, last_color_black=False, verbose=True)
-    new_cmap.append(rand_cmap(10, type=random.choice(['soft', 'bright']), first_color_black=False, last_color_black=False, verbose=True))
-    random.shuffle(new_cmap)
-
-    depth = 3
-    for i in range(0, depth):
-
+def get_image(seeds, save_to_folder, colormap):
+    print(colormap)
+    """ Fully deterministic function for reproducibly generating a single
+        Atomic NFT PNG from a list of random seeds. """
+    os.system("mkdir -p %s" % (save_to_folder))
+    for seed_index in range(len(seeds)):
+        # Generate component images, one for each seed. Save each component locally.
+        seed = seeds[seed_index]
         g = GenerativeImage(f1, f2)
-        seed = int(1000000000000 * random.random())
-        open("nfts/%d/%d_seed" % (nft_num ,i), "w").write(str(seed) + "\n")
         g.generate(seed=seed)
-        if i < 0:
-            g.plot(projection=Projection.POLAR, bgcolor="black", color=new_cmap[i])
-        else:
-            g.plot(projection=Projection.POLAR, color=new_cmap[i], bgcolor="transparent")
-        #plt.savefig('/tmp/%d.png' % (i), transparent=(i != 0))
-        g.save_image('nfts/%d/%d.png' % (nft_num, i), depth=8)
+        print("Using", colormap[seed_index])
+        g.plot(projection=Projection.POLAR, color=colormap[seed_index], bgcolor="transparent")
+        g.save_image('%s/seed_%d_%s.png' % (save_to_folder, seed_index, seed), depth=6)
 
-    background = Image.open("nfts/%d/0.png" % (nft_num))
-    for i in range(1, depth):
-        foreground = Image.open("nfts/%d/%d.png" % (nft_num, i))
+    # Merge components into a single transparent PNG
+    background = Image.open('%s/seed_0_%s.png' % (save_to_folder, seeds[0]))
+    for seed_index in range(1, len(seeds)):
+        foreground = Image.open("%s/seed_%d_%s.png" % (save_to_folder, seed_index, seeds[seed_index]))
         background.paste(foreground, (0, 0), foreground)
-    background.save("nfts/%d/image.png" % (nft_num))
+    background.save("%s/final_nft.png" % (save_to_folder))
+    print("Saved complete NFT to %s" % (save_to_folder))
 
+
+for nft_num in range(0, 200):
+    depth = int(random.random()) * 8 + 3
+    seeds = []
+    for i in range(0, depth):
+        seed = int(10000000000000000000000000000000000000000000000 * random.random())
+        seeds.append(seed)
+    get_image(seeds, "nfts/atomic_nfts_%d" % (nft_num), get_random_cmap())
 
     print("Finished NFT", nft_num)
---
+++ cmap_utils.py
@@ -0,0 +1,47 @@
+from samila.functions import is_valid_color as is_valid_color
+
+def rand_cmap(nlabels, type='bright', verbose=True):
+    """
+    Creates a random colormap to be used together with matplotlib. Useful for segmentation tasks
+    :param nlabels: Number of labels (size of colormap)
+    :param type: 'bright' for strong colors, 'soft' for pastel colors
+    :param verbose: Prints the number of labels and shows the colormap. True or False
+    :return: colormap for matplotlib
+    """
+    import colorsys
+    import numpy as np
+
+
+    if type not in ('bright', 'soft'):
+        print ('Please choose "bright" or "soft" for type')
+        return
+
+    if verbose:
+        print('Number of labels: ' + str(nlabels))
+
+    # Generate color map for bright colors, based on hsv
+    if type == 'bright':
+        randHSVcolors = [(np.random.uniform(low=0.0, high=1),
+                          np.random.uniform(low=0.2, high=1),
+                          np.random.uniform(low=0.9, high=1)) for i in range(nlabels)]
+
+        # Convert HSV list to RGB
+        randRGBcolors = []
+        for HSVcolor in randHSVcolors:
+            randRGBcolor = colorsys.hsv_to_rgb(HSVcolor[0], HSVcolor[1], HSVcolor[2])
+            if is_valid_color(randRGBcolor):
+                randRGBcolors.append(randRGBcolor)
+
+
+    # Generate soft pastel colors, by limiting the RGB spectrum
+    if type == 'soft':
+        low = 0.6
+        high = 0.95
+        randRGBcolors = list(filter(is_valid_color, [(np.random.uniform(low=low, high=high),
+                          np.random.uniform(low=low, high=high),
+                          np.random.uniform(low=low, high=high)) for i in range(nlabels)]))
+
+
+    for color in randRGBcolors:
+        if len(color) != 3:
+            randRGBcolors.remove(color)
